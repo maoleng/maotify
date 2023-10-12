ae677f84ed242d9c8bc695f798bb71e82965f040
Bùi Hữu Lộc <feature451@gmail.com>
2023-10-13 02:01:05 +0700

    fix: kernel command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('notify:run')->everyMinute();
+         $schedule->command('notify:fire')->everyMinute();
     }
 
     /**
